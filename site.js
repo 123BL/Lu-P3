f167fcce31fa3cf375b2949ca1f417b93b2c3ab2
Brandon Lu <123blu@frhsd.com>
2023-03-03 08:37:02 -0500

    almost finished

--- site.js
+++ site.js
@@ -47,12 +47,68 @@ const vue_app = Vue.createApp({
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
        getMonthText(dateArray) {
-            switch () {
-                  case movies[index].released = "[]"
-            }
-       }
+            switch (dateArray[1]) {
+                  case 0:
+                        return "Error";
+                        break;
+                        case 1:
+                        return "January" + " "+ dateArray[2]+ ", " + dateArray[0];
+                        break;
+                        case 2:
+                        return "Febuary" + " "+ dateArray[2]+ ", " + dateArray[0];
+                        break;
+                        case 3:
+                        return "March" + " "+ dateArray[2]+ ", " + dateArray[0];
+                        break;
+                        case 4:
+                        return "April" + " "+ dateArray[2]+ ", " + dateArray[0];
+                        break;
+                        case 5:
+                        return "May" + " "+ dateArray[2]+ ", " + dateArray[0];
+                        break;
+                        case 6:
+                        return "June" + " "+ dateArray[2]+ ", " + dateArray[0];
+                        break;
+                        case 7:
+                        return "July" + " "+ dateArray[2] +", " + dateArray[0];
+                        break;
+                        case 8:
+                        return "August" + " "+ dateArray[2]+ ", " + dateArray[0];
+                        break;
+                        case 9:
+                        return "September" + " "+ dateArray[2]+ ", " + dateArray[0];
+                        break;
+                        case 10:
+                        return "October" + " "+ dateArray[2]+ ", " + dateArray[0];
+                        break;
+                        case 11:
+                        return "November" + " "+ dateArray[2]+ ", " + dateArray[0];
+                        break;
+                        case 12:
+                        return "December" + " "+ dateArray[2] +", " + dateArray[0];
+                        break;
+                        default:
+                        return "Error";
 
-       }
+            }
+       },
+       posterClick(index){
+            var x = this.movie[index].posterindex;
+            var y = this.movie[index].poster.length;
+            if (x < y-1) {
+            this.movies[index].posterIndex++;
+            }
+                  else {
+                        this.movie[index].posterIndex = 0;
+                  }
+            },
+            timeText(minutes){
+             var hours = math.trunc(minutes/60);
+             var min = minutes % 60;
+             return hours + "h" + min + "m"
+            },
+       },
+      
 })
 
 
